72e1e603309f19f89250f6b0cb73a476a02fc914
Odenilson Marques <odenilson.marques@gmz.solutions>
2022-06-04 16:48:07 -0300

    inserindo imagem na pagina home e centralizando a imagem 404

--- telas/404.php
+++ telas/404.php
@@ -1,3 +1,4 @@
-<h1>Nada encontrado</h1>
-
+<h1 class="text-center mt-5">Nada encontrado</h1>
+<div class="text-center mt-5">
 <img src="img/404.svg" alt="imagem nao encontrada" width="25%" height="20%">
+</div>
--- telas/home.php
+++ telas/home.php
@@ -1,2 +1,4 @@
-<h1>Bem -vindo a pagina </h1><hr>
-<h2>Home</h2>
+<h1 class="text-center">Bem -vindo</h1>
+<div class="text-center mt-5">
+    <img src="img/agenda.svg" alt="imagem nao encontrada" width="25%" height="20%">
+</div>
